a5d416dc8a64b0ad59d69c9be62035abeb352556
Kvera Groove <kveragroove@gmail.com>
2019-04-17 06:58:41 -0300

    renderinado na tela

--- src/main.js
+++ src/main.js
@@ -1,3 +1,69 @@
+
+class App {
+    constructor() {
+        //arrei pra guardar a lista
+        this.repositories = []
+
+        this.formEl = document.getElementById("repo-from");
+        
+        this.listEl = document.getElementById('repo-list');
+        
+        this.registerHandlers();
+    }
+
+    registerHandlers() {
+        this.formEl.onsubmit = event => this.addRepository(event);
+    }
+
+    addRepository(event) {
+        event.preventDefault();
+
+        this.repositories.push({
+            name: 'curso-javascript-do-zero',
+            description: 'Código reproduzido durante o curso de Javascript do zero',
+            avatar_url: 'https://avatars0.githubusercontent.com/u/28929274?v-4',
+            html_url: 'https://github.com/Rocketseat/curso-javascript-do-zero',
+        });
+
+        this.render();
+    }
+
+    render() {
+        //limpa toda a lista
+        this.listEl.innerHTML = '';
+
+        this.repositories.forEach(repo => {
+            let imgEl = document.createElement('img');
+            imgEl.setAttribute('src', repo.avatar_url);
+            //atributo do bootstrap
+            imgEl.setAttribute('class', 'img-thumbnail col-4');
+
+            let titleEl = document.createElement('strong');
+            titleEl.appendChild(document.createTextNode(repo.name));
+
+            let descriptionEl = document.createElement('p');
+            descriptionEl.appendChild(document.createTextNode(repo.description));
+
+            let linkEl = document.createElement('a');
+            linkEl.setAttribute('target', '_blank');
+            linkEl.appendChild(document.createTextNode('Acessar'));
+
+            let listItemEl = document.createElement('li');
+            listItemEl.setAttribute('class', 'list-group-item list-group-item-action');
+            listItemEl.appendChild(imgEl);
+            listItemEl.appendChild(titleEl);
+            listItemEl.appendChild(descriptionEl);
+            listItemEl.appendChild(linkEl);
+
+            this.listEl.appendChild(listItemEl);
+        });
+
+    }
+}
+
+new App();
+
+/*
 import axios from 'axios';
 
 class Api {
@@ -12,3 +78,4 @@ class Api {
 }
 
 Api.getUserInfo('diego3')
+*/
